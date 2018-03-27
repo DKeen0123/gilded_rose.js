3a56a5a4f64c504409c31901be2d52818c3a5134
Daniel Keen <keendaniel01@gmail.com>
2018-03-27 14:02:01 +0100

    test: updateQuality() decrease item quality by 1 :white_check_mark:

--- spec/shop_spec.js
+++ spec/shop_spec.js
@@ -1,5 +1,6 @@
 describe('Shop', () => {
   let shop;
+  let bread;
 
   describe('#constructor', () => {
     beforeEach(() => {
@@ -8,6 +9,22 @@ describe('Shop', () => {
 
     it('initializes with an empty items array', () => {
       expect(shop.items).toEqual([]);
+    });
+  });
+
+  describe('#updateQuality', () => {
+    beforeEach(() => {
+      bread = new Item('Bread', 10, 8);
+      shop = new Shop([bread]);
+    });
+
+    describe('when it is a normal item', () => {
+      beforeEach(() => {
+        shop.updateQuality();
+      });
+      it('degrades quality of item by 1', () => {
+        expect(shop.items[0].quality).toEqual(7);
+      });
     });
   });
 });
